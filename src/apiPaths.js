73a1d3105bf1912b6f54dfa7397c2fc775219801
Karla Rocha <leonoemi123@gmail.com>
2023-06-23 19:48:32 -0700

    Updated APIs

--- src/apiPaths.js
+++ src/apiPaths.js
@@ -1,11 +1,9 @@
-const apiPaths = {
-  allMatches: '/matches/',
-  match: (id) => `/matches/${id}/`,
-  allPlayer: '/player/',
-  player: (id) => `/players/${id}/`,
-  allCells: '/cells/',
-  cell: (id) => `/cells/${id}/`,
-  playerMove: (id) => `/match/player-move/${id}/`,
+export const apiPaths = {
+  allMatches: 'http://localhost:8000/matches/',
+  match: (id) => `http://localhost:8000//matches/${id}/`,
+  allPlayer: 'http://localhost:8000//player/',
+  player: (id) => `http://localhost:8000//players/${id}/`,
+  allCells: 'http://localhost:8000//cells/',
+  cell: (id) => `http://localhost:8000//cells/${id}/`,
+  playerMove: (id) => `http://localhost:8000//match/player-move/${id}/`,
 };
-
-export default apiPaths;
