b7ec6325c93cefed243b977039cf7e97bc752926
Karla Rocha <leonoemi123@gmail.com>
2023-06-26 20:19:36 -0700

    Updated APIs

--- src/apiPaths.js
+++ src/apiPaths.js
@@ -1,10 +1,11 @@
 export const apiPaths = {
   allMatches: 'http://localhost:8000/matches/',
-  matchById: (id) => `http://localhost:8000//matches/${id}/`,
+  matchById: (id) => `http://localhost:8000/matches/${id}/`,
   match: 'http://localhost:8000/match/',
-  allPlayer: 'http://localhost:8000//player/',
-  playerById: (id) => `http://localhost:8000//players/${id}/`,
-  allCells: 'http://localhost:8000//cells/',
-  cellById: (id) => `http://localhost:8000//cells/${id}/`,
-  playerMove: (id) => `http://localhost:8000//match/player-move/${id}/`,
+  allPlayers: 'http://localhost:8000/players/',
+  playerById: (id) => `http://localhost:8000/players/${id}/`,
+  allCells: 'http://localhost:8000/cells/',
+  cellById: (id) => `http://localhost:8000/cells/${id}/`,
+  playerMove: (id) => `http://localhost:8000/match/player-move/${id}/`,
+  playerWinner: (id) => `http://localhost:8000/match/winner/${id}/`,
 };
